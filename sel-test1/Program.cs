92d99cbc37adbdcad8de629b159cc629155bfc95
Steve Oldfield <steveold1611@gmail.com>
2019-04-01 15:26:29 -0600

    Add project files.

---
+++ sel-test1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sel_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
