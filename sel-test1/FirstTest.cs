c032bbb59162a49f2008ca1c3e76c17da603ec47
Steve Oldfield <steveold1611@gmail.com>
2019-06-07 16:42:05 -0600

    added second page to testing

--- sel-test1/FirstTest.cs
+++ sel-test1/FirstTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+
 
 
 namespace sel_test1
@@ -15,27 +17,56 @@ namespace sel_test1
 
             driver.Manage().Window.Maximize();
 
-            IWebElement MoveFirstElement = driver.FindElement(By.XPath("//*[@id='sortable']/li[1]/span[text()='Item1']"));
-
+            IWebElement MoveFirstElement = driver.FindElement(By.XPath("//*[@id='sortable']//*[text()='Item 1']"));
             Actions builderFirst = new Actions(driver);
             IAction dragAndDropFirst = builderFirst.ClickAndHold(MoveFirstElement).MoveByOffset(0, 120).Release(MoveFirstElement).Build();
-
             dragAndDropFirst.Perform();
 
+            Thread.Sleep(2000);
             Console.WriteLine("test1 done");
 
-            IWebElement MoveLastElement = driver.FindElement(By.XPath("//*[@id='sortable']/li[7]/span"));
-
+            IWebElement MoveLastElement = driver.FindElement(By.XPath("//*[@id='sortable']//*[text()='Item 7']"));
             Actions builderLast = new Actions(driver);
             IAction dragAndDropLast = builderLast.ClickAndHold(MoveLastElement).MoveByOffset(0, -80).Release(MoveLastElement).Build();
             dragAndDropLast.Perform();
 
+            Thread.Sleep(2000);
             Console.WriteLine("test2 done");
 
+            // click on the Selectable link on sidebar: moves to the Selectable page
+            driver.FindElement(By.PartialLinkText("Selectable")).Click();
+
+            // select a selectable item
+            driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 1']")).Click();
+            Thread.Sleep(500);
+            driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 3']")).Click();
+            Thread.Sleep(500);
+            driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 7']")).Click();
+            Thread.Sleep(500);
+            // clear last selected item in prep for multiple selection 
+            var clearSelection = driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 7']"));
+            new Actions(driver).MoveToElement(clearSelection).MoveByOffset(-125, 10).Click().Perform();
+            
+            Thread.Sleep(2000);
+
+            Actions multiSelectBuild = new Actions(driver);
+            IWebElement MultiSelectFirst = driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 1']"));
+            IWebElement MultiSelectSecond = driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 3']"));
+            IWebElement MultiSelectThird = driver.FindElement(By.XPath("//*[@id='selectable']//*[text()='Item 5']"));
+
+            Actions multipleSelect = multiSelectBuild.KeyDown(Keys.Control).Click(MultiSelectFirst).Click(MultiSelectSecond).Click(MultiSelectThird);
+            multipleSelect.Perform();
+            Thread.Sleep(2000);
 
             Console.WriteLine("Test 3");
-            Console.WriteLine("Click the 'ANY' key to end the test");
-            Console.ReadKey();
+
+
+            // click on the Resizable link on sidebar: moves to the resizable page
+     //       driver.FindElement(By.PartialLinkText("resizable")).Click();
+            
+
+
+
 
 
             driver.Quit();
